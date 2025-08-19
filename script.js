716bd7b5eaacba0b5708995d397c5ec8e8e13bf6
Zeva-admin <imprayimpray4@gmail.com>
2025-08-20 04:36:33 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Добавим обработчики для кнопок "Купить"
-document.querySelectorAll('.buy-btn').forEach(function(button) {
-    button.addEventListener('click', function() {
-        // Замените ссылку на свой Telegram
-        window.location.href = 'https://t.me/your_telegram_link';
-    });
-});
