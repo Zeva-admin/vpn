7000d814b7e0b40abbea798869f89eb07bd399a2
Zeva-admin <imprayimpray4@gmail.com>
2025-08-20 05:13:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Переключение вкладок
+function showTab(tabName) {
+    const tabs = document.querySelectorAll('.tab-content');
+    const buttons = document.querySelectorAll('.tab-button');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(tabName).classList.add('active');
+    document.querySelector(`button[onclick="showTab('${tabName}')"]`).classList.add('active');
+}
